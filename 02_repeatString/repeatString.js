a1ac40e8f19ba764958ee3c45888c6446efea997
Anna Orlova <orlovanna412@gmail.com>
2024-01-12 19:17:18 +0000

    add solution for Exercise 02 and pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    let i = 1;
+    while (i <= num) {
+        output += string;
+        i++;
+    }
+    if (num < 0) {
+        output = 'ERROR';
+    }
+    return output;
 };
 
 // Do not edit below this line
