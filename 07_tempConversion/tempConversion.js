d8316c8dbfe292f8e34cde8a0406a685f015229b
Anna Orlova <orlovanna412@gmail.com>
2024-01-14 03:40:54 +0000

    add solution for Exercise 07 and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faTemp) {
+  return Math.round(((faTemp - 32) * 5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ceTemp) {
+  return Math.round((ceTemp * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
