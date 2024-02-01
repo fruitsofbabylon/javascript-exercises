07b78e188cb82edfc90f110434d7f26fab83ce52
Anna Orlova <orlovanna412@gmail.com>
2024-02-01 00:59:37 +0000

    add solution for 09 - Palindromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let str = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,'').toLowerCase();
+    let revStr = str.split('').reverse().join('');
+    return str == revStr ? true : false;
 };
 
 // Do not edit below this line
