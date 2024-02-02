93a001dfd4594a7d99c9abaa10b348dad228ea93
Anna Orlova <orlovanna412@gmail.com>
2024-02-02 01:05:06 +0000

    add solution for Exercise 10 - Fibonacci task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    index = Number(num);
+    if (index < 0) {
+        return 'OOPS';
+    } else {
+        let prevNum = 0;
+        let nextNum = 1;
+        let arr = [];
+        for (let i = 0; i <= 30; i ++) {
+            arr[i] = prevNum;                  
+            arr[i + 1] = prevNum + nextNum;    
+            let temp = nextNum;
+            nextNum += prevNum;                
+            prevNum = temp;
+        }
+        return arr[index];
+    }
 };
 
 // Do not edit below this line
