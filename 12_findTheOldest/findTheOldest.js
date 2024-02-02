5f8f6ef5d261868214040733f50153c8cac429cb
Anna Orlova <orlovanna412@gmail.com>
2024-02-02 02:14:31 +0000

    add solution for Exercise 12 - Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArr) {
+    return peopleArr.reduce((oldest, current) => {
+        if (!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        } else if (!current.yearOfDeath) {
+            current.yearOfDeath = new Date().getFullYear();
+        }
+        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let currentAge = current.yearOfDeath - current.yearOfBirth;
+        return oldestAge < currentAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
